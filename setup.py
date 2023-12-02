cc0dc92838db037d1765fb4d7141fad37d16390c
FullteaR <rikuta@furutan.com>
2023-12-02 11:58:01 +0900

    rename

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name = "leveldict",
+    name = "leveldictionary",
     version = "0.0.2",
     author = "Fulltea",
     author_email = "rikuta@furutan.com",
-    url = "https://github.com/FullteaR/LevelDict",
+    url = "https://github.com/FullteaR/LevelDictionary",
     install_requires = ["leveldb"],
 )
